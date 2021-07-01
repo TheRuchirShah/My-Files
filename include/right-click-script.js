25cad58b8ecf9232dd36f8d0db2cd9e1c63dd12f
Ruchir Shah <39954164+TheRuchirShah@users.noreply.github.com>
2021-07-01 19:25:36 +0530

    Add files via upload

---
+++ include/right-click-script.js
@@ -0,0 +1,9 @@
+<!-- Disable Right Click on Webpage/Website -->
+
+var message = "This Function is Disabled";
+
+function rtclickcheck(keyp){ if (navigator.appName == "Netscape" && keyp.which == 3){ alert(message); return false; }
+
+if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) { alert(message); return false; } }
+
+document.onmousedown = rtclickcheck;
